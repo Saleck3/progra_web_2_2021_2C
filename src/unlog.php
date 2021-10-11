c0230705bfbd6d7225213f0410f107489975d6f2
Alejandro Gonzalez <agonzalez@clacso.edu.ar>
2021-10-11 18:02:59 -0300

    Cambiado el repo a MVC

--- src/unlog.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once ("header.php");
-
-mensaje_al_usuario("exito", "Se deslogueo el usuario" .$_SESSION["usuario"]->getNombre);
-Usuario::desloguear();
-
-header('Location:index.php');
