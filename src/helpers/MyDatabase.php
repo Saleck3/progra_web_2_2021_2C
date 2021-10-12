6979edc8f9e4c4f5e7964ad2c2cd9329ae799e2b
Alejandro Gonzalez <agonzalez@clacso.edu.ar>
2021-10-11 22:58:04 -0300

    Agregada opcion de insert en base de datos

--- src/helpers/MyDatabase.php
+++ src/helpers/MyDatabase.php
@@ -1,30 +1,54 @@
 <?php
 
-class MyDatabase{
+class MyDatabase
+{
     private $connection;
-
-    public function __construct($servername, $username, $password, $dbname){
-        $this->connection = mysqli_connect($servername, $username,$password, $dbname);
-
+    
+    public function __construct($servername, $username, $password, $dbname)
+    {
+        $this->connection = mysqli_connect($servername, $username, $password, $dbname);
+        
         if (!$this->connection) {
             die("Connection failed: " . mysqli_connect_error());
         }
     }
-
-    public function 
-
-    public function __destruct(){
+    
+    public function __destruct()
+    {
         mysqli_close($this->connection);
     }
-
-    public function query($sql){
-        $databaseResult = mysqli_query($this->connection, $sql);
-        var_dump($databaseResult);
-        if (mysqli_num_rows($databaseResult) <= 0)
+    
+    /**
+     * @param $sql String Query (SELECT) a ejecutar en la base de datos
+     * @return array Set de datos o vacio en error
+     */
+    public function query($sql)
+    {
+        
+        if (!$databaseResult = mysqli_query($this->connection, $sql)) {
+            echo("ERROR");
+            var_dump($this->connection);
             return [];
-
-        return mysqli_fetch_all($databaseResult,MYSQLI_ASSOC);
+        }
+        
+        return mysqli_fetch_all($databaseResult, MYSQLI_ASSOC);
     }
-
-
+    
+    /**
+     * Este es para hacer inserts
+     *
+     * @param $sql String Insert a ejecutar en la base de datos
+     * @return bool Insert OK o error_list
+     */
+    public function insert($sql)
+    {
+        
+        if (!$databaseResult = mysqli_query($this->connection, $sql)) {
+            var_dump($this->connection);
+            return mysqli_error_list($this->connection);
+        }
+        
+        return $databaseResult;
+    }
+    
 }
