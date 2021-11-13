05d75f1bd066312199c40eb1cf228297e7364eba
Alejandro Gonzalez <agonzalez@clacso.edu.ar>
2021-11-13 15:14:57 -0300

    Primera iteracion traer reglas

--- src/controller/VuelosController.php
+++ src/controller/VuelosController.php
@@ -2,28 +2,43 @@
 
 class VuelosController
 {
-
+    
     private $vuelosModel;
     private $log;
     private $printer;
-
+    
     public function __construct($logger, $printer, $vuelosModel)
     {
-        $this->vuelosModel=$vuelosModel;
+        $this->vuelosModel = $vuelosModel;
         $this->log = $logger;
         $this->printer = $printer;
     }
-
+    
     function show()
     {
-        $data['vuelos']=$this->vuelosModel->getVuelos();
-
-        echo $this->printer->render("view/vuelosView.html",$data);
+        $data['vuelos'] = $this->vuelosModel->getVuelos();
+        echo $this->printer->render("view/vuelosView.html", $data);
     }
-
-    function buscar(){
-
-
+    
+    function buscar()
+    {
+    
+    
     }
-
+    
+    function suborbital()
+    {
+        
+        var_dump(date('Y-m-n'));
+        if (isset($_POST["date"])) {
+            $data['diaSeleccionado'] = $_POST["date"];
+            $data['vuelos'] = $this->vuelosModel->getVuelosDia($data["diaSeleccionado"]);
+            
+        } else {
+            $data['vuelos'] = $this->vuelosModel->getVuelos();
+        }
+        
+        $data["hoy"] = date('Y-m-n');
+        echo $this->printer->render("view/suborbitalView.html", $data);
+    }
 }
--- src/model/VuelosModel.php
+++ src/model/VuelosModel.php
@@ -3,15 +3,66 @@
 class VuelosModel
 {
     private $database;
-
+    
     public function __construct($database)
     {
         $this->database = $database;
     }
-
+    
     public function getVuelos()
     {
-        return $this->database->query("SELECT * FROM vuelo");
+        return $this->database->query("SELECT * FROM suborbitales");
     }
-
+    
+    /**
+     * @param $fecha
+     */
+    public function getVuelosDia($fecha)
+    {
+        $diaDeLaSemana = date('w', strtotime($fecha));
+        
+        //Cambio numero a nombre
+        switch ($diaDeLaSemana) {
+            case 0:
+            {
+                $diaDeLaSemana = 'Domingo';
+                break;
+            }
+            case 1:
+            {
+                $diaDeLaSemana = 'Lunes';
+                break;
+            }
+            case 2:
+            {
+                $diaDeLaSemana = 'Martes';
+                break;
+            }
+            case 3:
+            {
+                $diaDeLaSemana = 'Miercoles';
+                break;
+            }
+            case 4:
+            {
+                $diaDeLaSemana = 'Jueves';
+                break;
+            }
+            case 5:
+            {
+                $diaDeLaSemana = 'Viernes';
+                break;
+            }
+            case 6:
+            {
+                $diaDeLaSemana = 'Sabado';
+                break;
+            }
+            default:
+                $diaDeLaSemana = "dia";
+        }
+        //TODO Agregar filtro de lugar de salida
+        return $this->database->query("SELECT * FROM suborbitales where dia = '$diaDeLaSemana' ORDER BY horario");
+    }
+    
 }
