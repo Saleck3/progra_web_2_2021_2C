e81a62c010272b46d1929f4bbc8718c39e095c38
Leandro <leandro.ariel.gomez1@gmail.com>
2021-11-20 01:34:14 -0300

    agregue el codigo qr en el pdf

--- src/helpers/qrController.php
+++ src/helpers/qrController.php
@@ -10,8 +10,12 @@ class QrController{
     function generarQr(){
         $dato 	= 	time();
         $fileName = "public/imagenqr/qr_".$dato.".png";
-        QRcode::png($dato,$fileName,'L',10,5);
-        return $fileName;
+        QRcode::png($dato,$fileName,'L',5,1);
+
+        $type = pathinfo($fileName, PATHINFO_EXTENSION);
+        $data = file_get_contents($fileName);
+        $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+        return $base64;
     }
 
 }
