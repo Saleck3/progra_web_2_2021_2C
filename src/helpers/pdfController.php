546c686c5d53095f7035ce930e1800c2ee4d21ae
Alejandro Gonzalez <agonzalez@clacso.edu.ar>
2021-11-26 21:18:07 -0300

    Refactor de PDFController

--- src/helpers/pdfController.php
+++ src/helpers/pdfController.php
@@ -3,37 +3,37 @@
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
-class PdfController{
-
+class PdfController
+{
+    
     private $pdf;
-
-    public function __construct(){
-
+    
+    public function __construct()
+    {
+        
         require_once 'third-party/dompdf/autoload.inc.php';
         // instantiate and use the dompdf class
         $dompdf = new Dompdf();
-
+        
         // (Optional) Setup the paper size and orientation
         $dompdf->setPaper('A4', 'landscape');
-
+        
+        $options = new Options();
+        $options = $dompdf->getOptions();
+        $options->set(array('isRemoteEnabled' => TRUE));
+        $dompdf->setOptions($options);
         $this->pdf = $dompdf;
     }
-
-    public function generarPdf($nombrePdf,$html){
-
-        $options = new Options();
-        $options = $this->pdf->getOptions();
-        $options->set(array('isRemoteEnabled' => true));
-        $this->pdf->setOptions($options);
-
+    
+    public function generarPdf($nombrePdf, $html)
+    {
         $this->pdf->loadHtml($html);
-
+        
         $this->pdf->render();
         $output = $this->pdf->output();
-        file_put_contents("public/pdf/".$nombrePdf.".pdf",$output);
-
-        return $nombrePdf.".pdf";
-
+        file_put_contents("public/pdf/" . $nombrePdf . ".pdf", $output);
+        return $nombrePdf . ".pdf";
+        
         //$this->pdf->stream($nombrePdf.".pdf", array('Attachment' => false));
     }
 }
