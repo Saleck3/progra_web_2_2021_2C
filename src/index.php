3f68e3f8a3ead854e6d1127f623e09d3765254a1
Alejandro Gonzalez <agonzalez@clacso.edu.ar>
2021-11-30 15:43:09 -0300

    Agregada accion para debug

--- src/index.php
+++ src/index.php
@@ -1,12 +1,17 @@
 <?php
 session_start();
+if (isset($_SESSION["debug"])) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
 include_once("config/Configuration.php");
 
-$module = isset($_GET["module"]) ? $_GET["module"] : "home" ;
-$action = isset($_GET["action"]) ? $_GET["action"] : "show" ;
+$module = isset($_GET["module"]) ? $_GET["module"] : "home";
+$action = isset($_GET["action"]) ? $_GET["action"] : "show";
 
 $configuration = new Configuration();
-$router = $configuration->createRouter( "createHomeController", "show");
+$router = $configuration->createRouter("createHomeController", "show");
 
-$router->executeActionFromModule($module,$action);
+$router->executeActionFromModule($module, $action);
 
